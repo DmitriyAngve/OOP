15ae556ed0232d0e6808ebb781bceb10fd26e75a
Dmitriy Angve <kirirch129@gmail.com>
2022-11-14 13:16:16 +0300

    #

--- script.js
+++ script.js
@@ -523,7 +523,7 @@ tesla.accelerate();
 /////////////////////////////////////////////////////////////////////
 /////////////Inheritance Between "Classes": ES6 Classes//////////////
 /////////////////////////////////////////////////////////////////////
-
+/*
 class PersonCl {
   constructor(fullName, birthYear) {
     this.fullName = fullName;
@@ -626,3 +626,164 @@ StudentProto.introduce = function () {
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce(); // My name is Jay and I study Computer Science
 jay.calcAge(); // 27
+
+/////////////////////////////////////////////////////////////////////
+///////////////////////ANOTHER CLASS EXAMPLE/////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = []; // replace from argument
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111, []);
+
+// Bad idea (need to create method)
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000); // accessible
+
+console.log(acc1);
+console.log(acc1.pin); // accessible
+*/
+
+/////////////////////////////////////////////////////////////////////
+//////////Encapsulation: Protected Properties and Methods////////////
+/////////////////////////////////////////////////////////////////////
+/*
+// Underscore ahead of method make this method private
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // Protected property
+    this._pin = pin;
+    this._movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
+  deposit(val) {
+    this._movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111, []);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+console.log(acc1.pin);
+*/
+
+/////////////////////////////////////////////////////////////////////
+//////////Encapsulation: Private Class Fields and Methods////////////
+/////////////////////////////////////////////////////////////////////
+
+// Public fields
+// Private fields
+// Public methods
+// Private methods
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // Protected property
+    this._pin = pin;
+    this._movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
+  deposit(val) {
+    this._movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111, []);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+console.log(acc1.pin);
+
+////////////////////////////////////
